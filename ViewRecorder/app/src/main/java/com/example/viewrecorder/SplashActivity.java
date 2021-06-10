dadef54bdbaada45688d5973bcf0af4981c3e2c9
AvinashReddy9 <reddiavinahg@gmail.com>
2021-06-10 14:23:47 -0700

    Made UI changes and added splash screen

---
+++ ViewRecorder/app/src/main/java/com/example/viewrecorder/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.viewrecorder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+    }
+}
